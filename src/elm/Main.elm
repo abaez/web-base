c4f31e9e512266ef4e4db0dba09ac710d232065a
Alejandro Baez <code@baez.nyc>
2016-12-18 19:04:16 -0500

    main: small update for elm

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,4 +1,38 @@
-import Html exposing (text)
+module Main exposing (main)
+{-| The main module to use for the entire project.
+-}
+
+import Html exposing (Html, div, a, text, li, ul)
+import Html.Attributes exposing (id, href)
+
+type alias Url = String
+
+type alias Location =
+  { url: Url
+  , text: String
+  }
+
+getLocation : Location -> Html msg
+getLocation location =
+  li []
+    [ a
+      [ href location.url ]
+      [ text location.text ]
+    ]
+
 
 main =
-  text "Hello things"
+  div [id "locations"]
+    [
+      ul []
+        [ getLocation
+          { url = "https://twitter.com/a_baez"
+          , text = "Alejandro Baez"
+          }
+        , getLocation
+          { url = "http://brunch.io"
+          , text = "Brunch"
+          }
+        ]
+
+    ]
